2cf04b4517a6745c02ecdca737416d6629bbcc68
Bruno Zevallos <bruno_zevallos@usmp.pe>
2021-10-09 13:05:31 -0500

    Agregando Catálogo - Parte 02

---
+++ ajinomoto-app/Controllers/CatalogoController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using ajinomoto-app.Models;
+using ajinomoto-app.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity
+
+namespace ajinomoto_app.Controllers
+{
+    public class CatalogoController
+    {
+        
+    }
+}
