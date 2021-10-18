90a97f020a5d6a14ec63efbe33b3d8641b5f7692
Fabio Marangon <fabio_marangon@usmp.pe>
2021-10-17 23:38:04 -0500

    Agregando tabla pago

---
+++ ajinomoto-app/Models/Pago.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ajinomoto_app.Models
+{
+
+    [Table("t_pago")]
+    public class Pago
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        public DateTime FechaPago { get; set; }
+        public String NombreTarjeta { get; set; }
+        
+        [NotMapped]
+        public String NumeroTarjeta { get; set; }
+        
+        [NotMapped]
+        public String DueDateYYMM { get; set; }
+        
+        [NotMapped]
+        public String Cvv { get; set; }
+        public Decimal MontoTotal{ get; set; }
+        public String UserID{ get; set; }
+    }
+    
+}
