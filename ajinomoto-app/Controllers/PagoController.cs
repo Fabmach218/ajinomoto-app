c36c125d48358be1a75886ec42b814941337a038
Fabio Marangon <fabio_marangon@usmp.pe>
2021-10-26 21:53:12 -0500

    Creando método pago

--- ajinomoto-app/Controllers/PagoController.cs
+++ ajinomoto-app/Controllers/PagoController.cs
@@ -32,6 +32,50 @@ namespace ajinomoto_app.Controllers
             return View(pago);
         }
 
+        [HttpPost]
+        public IActionResult Pagar(Pago pago){
+            
+            pago.FechaPago = DateTime.Now;
+            _context.Add(pago);
+
+            var itemsProforma = from o in _context.DataProforma select o;
+            itemsProforma = itemsProforma.
+                Include(p => p.Producto).
+                Where(p => p.UserID.Equals(pago.UserID) && p.Status.Equals("Pendiente"));
+            
+            Pedido pedido = new Pedido();
+            pedido.UserID = pago.UserID;
+            pedido.Total = pago.MontoTotal;
+            pedido.Pago = pago;
+            pedido.Status = "Pendiente";
+            _context.Add(pedido);
+
+            List<DetallePedido> itemsPedido = new List<DetallePedido>();
+            foreach(var item in itemsProforma.ToList()){
+                DetallePedido detallePedido = new DetallePedido();
+                detallePedido.Pedido=pedido;
+                detallePedido.Precio = item.Price;
+                detallePedido.Producto = item.Producto;
+                detallePedido.Cantidad = item.Quantity;
+                itemsPedido.Add(detallePedido);
+            }
+
+            _context.AddRange(itemsPedido);
+
+            foreach (Proforma p in itemsProforma.ToList())
+            {
+                p.Status="Procesado";
+            }
+            _context.UpdateRange(itemsProforma);
+
+            _context.SaveChanges();
+
+            ViewData["Message"] = "El pago se ha registrado";
+            return View("Create");
+
+
+        }
+
 
     }
 }
