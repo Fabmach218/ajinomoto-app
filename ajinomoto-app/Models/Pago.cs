640b0fb44f15caaf3846d6cd40965de8a4688f7f
Kiara Choy <kiara_choy@usmp.pe>
2021-10-29 21:57:56 -0500

    pago model

--- ajinomoto-app/Models/Pago.cs
+++ ajinomoto-app/Models/Pago.cs
@@ -11,13 +11,20 @@ namespace ajinomoto_app.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("id")]
         public int Id { get; set; }
-        public DateTime PaymentDate { get; set; }
+
+        public DateTime FechaPago { get; set; }
         public String NombreTarjeta { get; set; }
+        
+        [NotMapped]
         public String NumeroTarjeta { get; set; }
+        
+        [NotMapped]
         public String DueDateYYMM { get; set; }
+        
+        [NotMapped]
         public String Cvv { get; set; }
         public Decimal MontoTotal{ get; set; }
         public String UserID{ get; set; }
-    }
+
     
 }
